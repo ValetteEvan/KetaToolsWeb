0fcc22d11784cd57d9e513a28ae907d05c4471c2
Valette Evan <113213514+ValetteEvan@users.noreply.github.com>
2025-02-13 08:38:10 +0100

    feat: create php files for connect to data base whith myPhpAdmin

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$servername = $_ENV['DB_SERVERNAME'];
+$username = $_ENV['DB_USERNAME'];
+$password = $_ENV['DB_PASSWORD'];
+$dbname = $_ENV['DB_NAME'];
+
+// Créez une connexion
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Vérifiez la connexion
+if ($conn->connect_error) {
+    die("La connexion a échoué: " . $conn->connect_error);
+}
+?>
--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 document.getElementById("btn1").addEventListener("click", function() {
     document.getElementById("form-container1").style.display = "block";  // Correction de l'ID ici
     document.getElementById("form-container2").style.display = "none";  // Cacher l'autre formulaire
@@ -7,4 +6,14 @@ document.getElementById("btn1").addEventListener("click", function() {
 document.getElementById("btn2").addEventListener("click", function() {
     document.getElementById("form-container2").style.display = "block";  // Affiche formulaire 2
     document.getElementById("form-container1").style.display = "none";  // Cacher l'autre formulaire
+});
+
+document.getElementById('btn-login').addEventListener('click', function() {
+    document.getElementById('form-login').style.display = 'block';
+    document.getElementById('form-register').style.display = 'none';
+});
+
+document.getElementById('btn-register').addEventListener('click', function() {
+    document.getElementById('form-register').style.display = 'block';
+    document.getElementById('form-login').style.display = 'none';
 });
